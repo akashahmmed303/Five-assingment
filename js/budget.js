a36a90954e8dbb1a6e0b62a84a0de288369a1fe1
akashahmmed303 <ahmedakash336@gmail.com>
2022-08-23 08:21:00 +0600

    manager & coach value added

--- js/budget.js
+++ js/budget.js
@@ -6,7 +6,28 @@ document.getElementById("calculate"),
 
     const expensesField = document.getElementById("expenses");
     const expensesFieldString = expensesField.innerText;
-    const expenses = parseFloat(perPlayerString);
+    const expenses = parseFloat(expensesFieldString);
+
+    expensesField.innerText = perPlayer * 5;
+  });
+
+document.getElementById("calculate-total"),
+  addEventListener("click", function () {
+    const managerField = document.getElementById("manager-field");
+    const managerFieldString = managerField.value;
+    const manager = parseFloat(managerFieldString);
+
+    const coachField = document.getElementById("coach-field");
+    const coachFieldString = coachField.value;
+    const coach = parseFloat(coachFieldString);
+
+    const perPlayerField = document.getElementById("per-player-field");
+    const perPlayerString = perPlayerField.value;
+    const perPlayer = parseFloat(perPlayerString);
+
+    const expensesField = document.getElementById("expenses");
+    const expensesFieldString = expensesField.innerText;
+    const expenses = parseFloat(expensesFieldString);
 
     expensesField.innerText = perPlayer * 5;
   });
